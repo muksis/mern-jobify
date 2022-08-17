383335830aa5f81efbe0ccddbef9b62ee43d8e87
muksis <60219890+muksis@users.noreply.github.com>
2022-08-17 13:13:10 -0400

    Add landing page structure

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,29 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <h4>Landing Page</h4>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, hic
+            esse. Velit, non sint tempora numquam illum perferendis repellat
+            eligendi voluptatibus. Laborum, quibusdam? Harum laborum
+            exercitationem autem, a unde ipsum.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
