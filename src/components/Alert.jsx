af58d8cec971f82d289834b9bebc6f878a3f2495
muksis <60219890+muksis@users.noreply.github.com>
2022-09-14 17:09:15 -0400

    Add alert display complete

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,7 +2,7 @@ import { useAppContext } from '../context/AppContext';
 
 const Alert = () => {
   const { alertType, alertText } = useAppContext();
-  return <div className={'alert alert-${alertType}'}>{alertText}</div>;
+  return <div className={`alert alert-${alertType}`}>{alertText}</div>;
 };
 
 export default Alert;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,15 +14,20 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
-  const { isLoading, showAlert } = useAppContext();
+  const { isLoading, showAlert, displayAlert } = useAppContext();
 
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+    console.log(values);
   };
 
   const toggleMember = () => {
