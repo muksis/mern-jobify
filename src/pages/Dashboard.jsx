f76d7e48ba23a323bfac7d176b01cead14bbbdb9
muksis <60219890+muksis@users.noreply.github.com>
2022-10-27 16:29:22 -0400

    Add fetch error

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div>Dashboard</div>;
 };
 
