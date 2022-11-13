a1a6c0142376434670e8438c3c0fc0c1f8ccfd6b
muksis <60219890+muksis@users.noreply.github.com>
2022-11-13 14:13:09 -0500

    Refactor login user

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,7 +14,7 @@ const initialState = {
 const Register = () => {
   const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
+  const { user, isLoading, showAlert, displayAlert, setupUser } =
     useAppContext();
 
   const handleChange = (e) => {
@@ -30,9 +30,17 @@ const Register = () => {
     }
     const currentUser = { name, email, password };
     if (isMember) {
-      loginUser(currentUser);
+      setupUser({
+        currentUser,
+        endPoint: 'login',
+        alertText: 'Successful login! Redirecting..',
+      });
     } else {
-      registerUser(currentUser);
+      setupUser({
+        currentUser,
+        endPoint: 'register',
+        alertText: 'User created! Redirecting..',
+      });
     }
   };
 
