d50f7c0fd9465f373f679f2a675b47f3b905d986
muksis <60219890+muksis@users.noreply.github.com>
2022-11-17 15:35:30 -0500

    Add protected route

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { useAppContext } from '../context/AppContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAppContext();
+
+  if (!user) {
+    return <Navigate to='/landing' />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
