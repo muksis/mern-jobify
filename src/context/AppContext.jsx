837abc354d0be60491eb7ca5a92a58483156473e
muksis <60219890+muksis@users.noreply.github.com>
2022-12-15 09:40:36 -0500

    Add axios manual approach

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -87,6 +87,23 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage();
   };
 
+  const updateUser = async (currentUser) => {
+    try {
+      const { data } = await axios.patch(
+        '/api/v1/auth/updateUser',
+        currentUser,
+        {
+          headers: {
+            Authorization: `Bearer ${state.token}`,
+          },
+        }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   return (
     <AppContext.Provider
       value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser }}
