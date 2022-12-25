e97034ea56d097bda30fe31c1d759389fa6b2d6d
muksis <60219890+muksis@users.noreply.github.com>
2022-12-25 13:35:35 -0500

    Add job page structure

--- src/pages/dashboard/AddJob.jsx
+++ src/pages/dashboard/AddJob.jsx
@@ -1,5 +1,31 @@
+import { FormRow, Alert } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../..assets/wrappers/DashboardFormPage';
+
 const AddJob = () => {
-  return <h1>Add Job Page</h1>;
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    staus,
+    statusOptions,
+  } = useAppContext();
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+        {showAlert && <Alert />}
+        <div className='form-center'>
+          <FormRow type='text' name='position' value={position} />
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
