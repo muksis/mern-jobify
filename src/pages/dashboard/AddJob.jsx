33f12583cb9ebffb13645ae9f9e7bd8e88749bb7
muksis <60219890+muksis@users.noreply.github.com>
2022-12-30 11:45:51 -0500

    Add job type select

--- src/pages/dashboard/AddJob.jsx
+++ src/pages/dashboard/AddJob.jsx
@@ -1,6 +1,6 @@
 import { FormRow, Alert } from '../../components';
-import { useAppContext } from '../../context/appContext';
-import Wrapper from '../..assets/wrappers/DashboardFormPage';
+import { useAppContext } from '../../context/AppContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
 
 const AddJob = () => {
   const {
@@ -62,6 +62,26 @@ const AddJob = () => {
             handleChange={handleJobInput}
           />
           {/* job type */}
+          <div className='form-row'>
+            <label htmlFor='jobType' className='form-label'>
+              job type
+            </label>
+
+            <select
+              name='jobType'
+              value={jobType}
+              onChange={handleJobInput}
+              className='form-select'
+            >
+              {jobTypeOptions.map((itemValue, index) => {
+                return (
+                  <option key={index} value={itemValue}>
+                    {itemValue}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
           {/* job status */}
           <div className='btn-container'>
             <button
